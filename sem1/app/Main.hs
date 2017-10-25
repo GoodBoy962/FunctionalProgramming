b552481ab71cf86a39d599451630575cd08415c6
Alexander Pliskin <alexnetmore@gmail.com>
2017-10-25 22:59:06 +0300

    move TermP in Term file

--- sem1/app/Main.hs
+++ sem1/app/Main.hs
@@ -1,36 +1,8 @@
 module Main where
 
-import Term (Symbol (..), TermS (..), TermP (..), sym, lam, app)
+import Term (Symbol (..), TermS (..), TermP (..), sym, lam, app, toTermS, tru, fls, iff', not', or', and', cons', head')
 import AlphaConversion (alpha)
 import BetaReduction (beta)
-
-tru = lam "t" $ lam "f" $ sym "t"
-fls = lam "t" $ lam "f" $ sym "f"
-iff' b x y = app (app (app (lam "b" $ lam "x" $ lam "y" $ app (app (sym "b") (sym "x")) (sym "y")) (b)) (x)) (y)
-not' x = lam "x" $ app (app (x) (fls)) (tru)
-or' x y = lam "x" $ lam "y" $ app (app (x) (tru)) (y)
-and' x y = lam "x" $ lam "y" $ app (app (x) (y)) (fls)
--- list'
--- cons'
-isNil' t = app (lam "c" $ lam "n" $ sym "n") (toTermS t)
--- head'
--- tail'
-termP p = TermP p
-
-toTermS :: TermP -> TermS
--- (2.1) list
--- toTermS (Cons term1 term2) =
-toTermS (IsNil term) = isNil' term
--- toTermS (Head term) =
--- toTermS (Tail term) =
--- (2.2) bool
-toTermS (Boolean b) = if b then tru else fls
-toTermS (Iff b term1 term2) = iff' (toTermS (Boolean (b))) (toTermS term1) (toTermS term2)
-toTermS (Not term) = not' (toTermS term)
-toTermS (And term1 term2) = and' (toTermS term1) (toTermS term2)
-toTermS (Or term1 term2) = or' (toTermS term1) (toTermS term2)
---
-toTermS (TermP term) = term
 
 solve :: TermP -> Maybe TermS
 solve = beta . alpha . toTermS
--- sem1/app/Term.hs
+++ sem1/app/Term.hs
@@ -1,4 +1,5 @@
-module Term (Symbol (..), TermS (..), TermP (..), sym, lam, app) where
+module Term (
+Symbol (..), TermS (..), TermP (..), sym, lam, app, toTermS, tru, fls, iff', not', or', and', cons', head') where
 
 newtype Symbol = Symbol { unSymbol :: String } deriving (Eq,Read)
 
@@ -23,7 +24,7 @@ app t1 t2 = AppS t1 t2
 data TermP = TermP TermS
             -- (3)
             | Boolean Bool
-            | Iff Bool TermP TermP
+            | Iff TermP TermP TermP
             | Not TermP
             | And TermP TermP
             | Or TermP TermP
@@ -48,3 +49,29 @@ data TermP = TermP TermS
             | Head TermP
             | Tail TermP
             deriving (Eq,Show,Read)
+
+tru = lam "t" $ lam "f" $ sym "t"
+fls = lam "t" $ lam "f" $ sym "f"
+iff' b x y = app (app (app (lam "b" $ lam "x" $ lam "y" $ app (app (sym "b") (sym "x")) (sym "y")) (b)) (x)) (y)
+not' x = lam "x" $ app (app (x) (fls)) (tru)
+or' x y = lam "x" $ lam "y" $ app (app (x) (tru)) (y)
+and' x y = lam "x" $ lam "y" $ app (app (x) (y)) (fls)
+
+cons' = lam "h" $ lam "t" $ lam "c" $ lam "n" $ app (app (sym "c") (sym "h")) (app (app (sym "t") (sym "c")) (sym "n"))
+-- nil' t = app (lam "c" $ lam "n" $ sym "n") (toTermS t)
+-- isNil' t =
+head' = lam "l" $ app (app (sym "l") (lam "h" $ lam "t" $ sym "h")) (fls)
+-- tail' =
+termP p = TermP p
+
+toTermS :: TermP -> TermS
+-- toTermS (Cons term1 term2) =
+-- toTermS (IsNil term) = isNil' term
+-- toTermS (Head term) =
+-- toTermS (Tail term) =
+toTermS (Boolean b) = if b then tru else fls
+toTermS (Iff b term1 term2) = iff' (toTermS b) (toTermS term1) (toTermS term2)
+toTermS (Not term) = not' (toTermS term)
+toTermS (And term1 term2) = and' (toTermS term1) (toTermS term2)
+toTermS (Or term1 term2) = or' (toTermS term1) (toTermS term2)
+toTermS (TermP term) = term
